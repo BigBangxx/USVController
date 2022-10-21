0600aa617e32ce7006b30116d348c3dff41fa888
桑彤彤 <sangtongtong@qq.com>
2022-10-21 17:17:00 +0800

    fix formation bug

--- GNC/control.py
+++ GNC/control.py
@@ -26,7 +26,10 @@ class Control:
         self.__update()
         self.__choose_mode()
         self.__control()
-        Gcs_command['average_index'] = Gcs_command['index_sum'] / Gcs_command['ship_num']
+        if Gcs_command['ship_num'] == 0:
+            Gcs_command['average_index'] = 0
+        else:
+            Gcs_command['average_index'] = Gcs_command['index_sum'] / Gcs_command['ship_num']
 
     def __update(self):
         self.point_current = Point(Nav_data['location']['latitude'],
